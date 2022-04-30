45f8f7271cab8c62ff87379c8817958d9ee2fb7d
Alexander Neupokoev <admin@armavir.ru>
2022-04-30 21:57:14 +0300

    fix

--- solution.go
+++ solution.go
@@ -23,7 +23,7 @@ func CalcSquare(sideLen float64, sidesNum intCustomType) float64 {
 	case 4:
 		return float64(sideLen * sideLen)
 	case 0:
-		return float64(math.Pi * 2 * sideLen)
+		return float64(math.Pi * 2 * sideLen * sideLen)
 	default:
 		return float64(0)
 	}
